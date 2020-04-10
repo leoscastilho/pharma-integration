b2674f81da387ce704426cf9842a37634ccf0933
Castilho <leonardo@kissolutions.tech>
2020-04-10 09:19:12 -0300

    Fix database connection

--- src/main/java/com/estee/na/app/Application.java
+++ src/main/java/com/estee/na/app/Application.java
@@ -2,8 +2,10 @@ package com.estee.na.app;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
